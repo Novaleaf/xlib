cec9c8b53519d4d334ab5ee76c6e6167657bbab8
Jason <JasonS@Novaleaf.com>
2016-09-30 19:01:33 -0700

    copy Axios definition to allow "npm link" workaround while typescript has it's compile bug.

--- dist/net.d.ts
+++ dist/net.d.ts
@@ -1,10 +1,127 @@
-/// <reference types="axios" />
 /// <reference types="bluebird" />
 /** the axios httpClient library:  https://github.com/mzabriskie/axios */
 import * as axios from "axios";
 export { axios };
 import * as promise from "./promise";
 import Promise = promise.bluebird;
+/**
+ * HTTP Basic auth details
+ */
+export interface AxiosHttpBasicAuth {
+    username: string;
+    password: string;
+}
+/**
+ * Common axios XHR config interface
+ * <T> - request body data type
+ */
+export interface AxiosXHRConfigBase<T> {
+    /**
+     * will be prepended to `url` unless `url` is absolute.
+     * It can be convenient to set `baseURL` for an instance
+     * of axios to pass relative URLs to methods of that instance.
+     */
+    baseURL?: string;
+    /**
+     * custom headers to be sent
+     */
+    headers?: Object;
+    /**
+     * URL parameters to be sent with the request
+     */
+    params?: Object;
+    /**
+     * optional function in charge of serializing `params`
+     * (e.g. https://www.npmjs.com/package/qs, http://api.jquery.com/jquery.param/)
+     */
+    paramsSerializer?: (params: Object) => string;
+    /**
+     * specifies the number of milliseconds before the request times out.
+     * If the request takes longer than `timeout`, the request will be aborted.
+     */
+    timeout?: number;
+    /**
+     * indicates whether or not cross-site Access-Control requests
+     * should be made using credentials
+     */
+    withCredentials?: boolean;
+    /**
+     * indicates that HTTP Basic auth should be used, and supplies
+     * credentials. This will set an `Authorization` header,
+     * overwriting any existing `Authorization` custom headers you have
+     * set using `headers`.
+     */
+    auth?: AxiosHttpBasicAuth;
+    /**
+     * indicates the type of data that the server will respond with
+     * options are 'arraybuffer', 'blob', 'document', 'json', 'text'
+     */
+    responseType?: string;
+    /**
+     * name of the cookie to use as a value for xsrf token
+     */
+    xsrfCookieName?: string;
+    /**
+     * name of the http header that carries the xsrf token value
+     */
+    xsrfHeaderName?: string;
+    /**
+     * Change the request data before it is sent to the server.
+     * This is only applicable for request methods 'PUT', 'POST', and 'PATCH'
+     * The last function in the array must return a string or an ArrayBuffer
+     */
+    transformRequest?: (<U>(data: T) => U) | [<U>(data: T) => U];
+    /**
+     * change the response data to be made before it is passed to then/catch
+     */
+    transformResponse?: <U>(data: T) => U;
+}
+/**
+ * <T> - request body data type
+ */
+export interface AxiosXHRConfig<T> extends AxiosXHRConfigBase<T> {
+    /**
+     * server URL that will be used for the request, options are:
+     * GET, PUT, POST, DELETE, CONNECT, HEAD, OPTIONS, TRACE, PATCH
+     */
+    url: string;
+    /**
+     * request method to be used when making the request
+     */
+    method?: string;
+    /**
+     * data to be sent as the request body
+     * Only applicable for request methods 'PUT', 'POST', and 'PATCH'
+     * When no `transformRequest` is set, must be a string, an ArrayBuffer or a hash
+     */
+    data?: T;
+}
+/**
+ * <T> - expected response type,
+ * <U> - request body data type
+ */
+export interface AxiosXHR<T> {
+    /**
+     * Response that was provided by the server
+     */
+    data: T;
+    /**
+     * HTTP status code from the server response
+     */
+    status: number;
+    /**
+     * HTTP status message from the server response
+     */
+    statusText: string;
+    /**
+     * headers that the server responded with
+     */
+    headers: Object;
+    /**
+     * config that was provided to `axios` for the request
+     */
+    config: AxiosXHRConfig<T>;
+}
 /**
 *  a helper for constructing reusable endpoint functions
 */
@@ -14,32 +131,32 @@ export declare class EzEndpointFunction<TSubmitPayload, TRecievePayload> {
     /** default is to retry for up to 10 seconds, (no retries after 10 seconds) */
     retryOptions: promise._BluebirdRetryInternals.IOptions;
     /** default is to timeout (err 545) after 60 seconds*/
-    requestOptions: Axios.AxiosXHRConfigBase<TRecievePayload>;
+    requestOptions: AxiosXHRConfigBase<TRecievePayload>;
     /** allows aborting retries (if any).
     return a Promise.reject() to ABORT RETRY (stop immediately with the error passed to reject())
     return a Promise.resolve() to signal that the request should be retried.
     NOTE:   error's of statusCode 545 are request timeouts
     DEFAULT:  by default we will retry error 500 and above. */
-    preRetryErrorIntercept: (err: Axios.AxiosXHR<TRecievePayload>) => Promise<void>;
+    preRetryErrorIntercept: (err: AxiosXHR<TRecievePayload>) => Promise<void>;
     constructor(origin?: string, path?: string, 
         /** default is to retry for up to 10 seconds, (no retries after 10 seconds) */
         retryOptions?: promise._BluebirdRetryInternals.IOptions, 
         /** default is to timeout (err 545) after 60 seconds*/
-        requestOptions?: Axios.AxiosXHRConfigBase<TRecievePayload>, 
+        requestOptions?: AxiosXHRConfigBase<TRecievePayload>, 
         /** allows aborting retries (if any).
         return a Promise.reject() to ABORT RETRY (stop immediately with the error passed to reject())
         return a Promise.resolve() to signal that the request should be retried.
         NOTE:   error's of statusCode 545 are request timeouts
         DEFAULT:  by default we will retry error 500 and above. */
-        preRetryErrorIntercept?: (err: Axios.AxiosXHR<TRecievePayload>) => Promise<void>);
+        preRetryErrorIntercept?: (err: AxiosXHR<TRecievePayload>) => Promise<void>);
     toJson(): {
         origin: string | undefined;
         path: string | undefined;
         retryOptions: promise._BluebirdRetryInternals.IOptions;
-        requestOptions: Axios.AxiosXHRConfigBase<TRecievePayload>;
+        requestOptions: AxiosXHRConfigBase<TRecievePayload>;
     };
     post(submitPayload?: TSubmitPayload, 
-        /**setting a key overrides the key put in ctor.requestOptions. */ customRequestOptions?: Axios.AxiosXHRConfigBase<TRecievePayload>, customOrigin?: string | undefined, customPath?: string | undefined): Promise<Axios.AxiosXHR<TRecievePayload>>;
+        /**setting a key overrides the key put in ctor.requestOptions. */ customRequestOptions?: AxiosXHRConfigBase<TRecievePayload>, customOrigin?: string | undefined, customPath?: string | undefined): Promise<AxiosXHR<TRecievePayload>>;
     get(
-        /**setting a key overrides the key put in ctor.requestOptions. */ customRequestOptions?: Axios.AxiosXHRConfigBase<TRecievePayload>, customOrigin?: string | undefined, customPath?: string | undefined): Promise<Axios.AxiosXHR<TRecievePayload>>;
+        /**setting a key overrides the key put in ctor.requestOptions. */ customRequestOptions?: AxiosXHRConfigBase<TRecievePayload>, customOrigin?: string | undefined, customPath?: string | undefined): Promise<AxiosXHR<TRecievePayload>>;
 }
--- src/net.ts
+++ src/net.ts
@@ -21,6 +21,141 @@ import * as stringHelper from "./stringhelper";
 
 
 /**
+ * HTTP Basic auth details
+ */
+export interface AxiosHttpBasicAuth {
+	username: string;
+	password: string;
+}
+	/**
+	 * Common axios XHR config interface
+	 * <T> - request body data type
+	 */
+export	interface AxiosXHRConfigBase<T> {
+	/**
+	 * will be prepended to `url` unless `url` is absolute.
+	 * It can be convenient to set `baseURL` for an instance
+	 * of axios to pass relative URLs to methods of that instance.
+	 */
+	baseURL?: string;
+
+	/**
+	 * custom headers to be sent
+	 */
+	headers?: Object;
+
+	/**
+	 * URL parameters to be sent with the request
+	 */
+	params?: Object;
+
+	/**
+	 * optional function in charge of serializing `params`
+	 * (e.g. https://www.npmjs.com/package/qs, http://api.jquery.com/jquery.param/)
+	 */
+	paramsSerializer?: (params: Object) => string;
+
+	/**
+	 * specifies the number of milliseconds before the request times out.
+	 * If the request takes longer than `timeout`, the request will be aborted.
+	 */
+	timeout?: number;
+
+	/**
+	 * indicates whether or not cross-site Access-Control requests
+	 * should be made using credentials
+	 */
+	withCredentials?: boolean;
+
+	/**
+	 * indicates that HTTP Basic auth should be used, and supplies
+	 * credentials. This will set an `Authorization` header,
+	 * overwriting any existing `Authorization` custom headers you have
+	 * set using `headers`.
+	 */
+	auth?: AxiosHttpBasicAuth;
+
+	/**
+	 * indicates the type of data that the server will respond with
+	 * options are 'arraybuffer', 'blob', 'document', 'json', 'text'
+	 */
+	responseType?: string;
+
+	/**
+	 * name of the cookie to use as a value for xsrf token
+	 */
+	xsrfCookieName?: string;
+
+	/**
+	 * name of the http header that carries the xsrf token value
+	 */
+	xsrfHeaderName?: string;
+
+	/**
+	 * Change the request data before it is sent to the server.
+	 * This is only applicable for request methods 'PUT', 'POST', and 'PATCH'
+	 * The last function in the array must return a string or an ArrayBuffer
+	 */
+	transformRequest?: (<U>(data: T) => U) | [<U>(data: T) => U];
+
+	/**
+	 * change the response data to be made before it is passed to then/catch
+	 */
+	transformResponse?: <U>(data: T) => U;
+}
+/**
+ * <T> - request body data type
+ */
+export interface AxiosXHRConfig<T> extends AxiosXHRConfigBase<T> {
+	/**
+	 * server URL that will be used for the request, options are:
+	 * GET, PUT, POST, DELETE, CONNECT, HEAD, OPTIONS, TRACE, PATCH
+	 */
+	url: string;
+
+	/**
+	 * request method to be used when making the request
+	 */
+	method?: string;
+
+	/**
+	 * data to be sent as the request body
+	 * Only applicable for request methods 'PUT', 'POST', and 'PATCH'
+	 * When no `transformRequest` is set, must be a string, an ArrayBuffer or a hash
+	 */
+	data?: T;
+}
+/**
+ * <T> - expected response type,
+ * <U> - request body data type
+ */
+export interface AxiosXHR<T> {
+	/**
+	 * Response that was provided by the server
+	 */
+	data: T;
+
+	/**
+	 * HTTP status code from the server response
+	 */
+	status: number;
+
+	/**
+	 * HTTP status message from the server response
+	 */
+	statusText: string;
+
+	/**
+	 * headers that the server responded with
+	 */
+	headers: Object;
+
+	/**
+	 * config that was provided to `axios` for the request
+	 */
+	config: AxiosXHRConfig<T>;
+}
+/**
 *  a helper for constructing reusable endpoint functions
 */
 export class EzEndpointFunction<TSubmitPayload, TRecievePayload>{
@@ -31,13 +166,13 @@ export class EzEndpointFunction<TSubmitPayload, TRecievePayload>{
 		/** default is to retry for up to 10 seconds, (no retries after 10 seconds) */
 		public retryOptions: promise._BluebirdRetryInternals.IOptions = { timeout: 60000, interval: 100, backoff: 2, max_interval: 5000 },
 		/** default is to timeout (err 545) after 60 seconds*/
-		public requestOptions: Axios.AxiosXHRConfigBase<TRecievePayload> = { timeout: 60000 },
+		public requestOptions: AxiosXHRConfigBase<TRecievePayload> = { timeout: 60000 },
 		/** allows aborting retries (if any).
 		return a Promise.reject() to ABORT RETRY (stop immediately with the error passed to reject())
 		return a Promise.resolve() to signal that the request should be retried.        
 		NOTE:   error's of statusCode 545 are request timeouts
 		DEFAULT:  by default we will retry error 500 and above. */
-		public preRetryErrorIntercept: (err: Axios.AxiosXHR<TRecievePayload>) => Promise<void> = (err) => {
+		public preRetryErrorIntercept: (err: AxiosXHR<TRecievePayload>) => Promise<void> = (err) => {
 			if (err.status <= 499) {
 				//console.assert(false, "err");					
 				return Promise.reject(err);
@@ -53,16 +188,16 @@ export class EzEndpointFunction<TSubmitPayload, TRecievePayload>{
 		return { origin: this.origin, path: this.path, retryOptions: this.retryOptions, requestOptions: this.requestOptions };
 	}
 
-    public post(
-        submitPayload?: TSubmitPayload,
-        /**setting a key overrides the key put in ctor.requestOptions. */customRequestOptions?: Axios.AxiosXHRConfigBase<TRecievePayload>,
-        customOrigin: string|undefined  = this.origin,
-        customPath: string | undefined = this.path
-    ): Promise<Axios.AxiosXHR<TRecievePayload>> {
+	public post(
+		submitPayload?: TSubmitPayload,
+		/**setting a key overrides the key put in ctor.requestOptions. */customRequestOptions?: AxiosXHRConfigBase<TRecievePayload>,
+		customOrigin: string|undefined  = this.origin,
+		customPath: string | undefined = this.path
+	): Promise<AxiosXHR<TRecievePayload>> {
 		log.debug("EzEndpointFunction .post() called");
 		let lastErrorResult: any = null;
-		return promise.retry<Axios.AxiosXHR<TRecievePayload>>(() => {
-            
+		return promise.retry<AxiosXHR<TRecievePayload>>(() => {
+			
 			try {
 
 				log.debug("EzEndpointFunction .post() in promise.retry block");
@@ -70,7 +205,7 @@ export class EzEndpointFunction<TSubmitPayload, TRecievePayload>{
 				//log.debug("EzEndpointFunction axios.post", { endpoint });
 
 
-				let finalRequestOptions: Axios.AxiosXHRConfigBase<TRecievePayload>;
+				let finalRequestOptions: AxiosXHRConfigBase<TRecievePayload>;
 				if (customRequestOptions == null || Object.keys(customRequestOptions).length === 0) {
 					finalRequestOptions = this.requestOptions;
 				} else {
@@ -78,11 +213,11 @@ export class EzEndpointFunction<TSubmitPayload, TRecievePayload>{
 				}
 
 				return (axios.post<TRecievePayload>(endpoint, submitPayload, finalRequestOptions
-				) as any as Promise<Axios.AxiosXHR<TRecievePayload>>)
+				) as any as Promise<AxiosXHR<TRecievePayload>>)
 					.then((result) => {
 						log.debug("EzEndpointFunction .post() got valid response");
 						return Promise.resolve(result);
-					}, (err: Axios.AxiosXHR<TRecievePayload>) => {
+					}, (err: AxiosXHR<TRecievePayload>) => {
 						log.debug("EzEndpointFunction .post() got err");
 						//log.info(err);
 						if (err.status === 0 && err.statusText === "" && err.data === "" as any) {
@@ -99,14 +234,14 @@ export class EzEndpointFunction<TSubmitPayload, TRecievePayload>{
 							if (interceptedErrorResult.isFulfilled() === false) {
 								throw log.error("EzEndpointFunction POST interceptResult() promise is not fulfilled", { submitPayload, interceptedErrorResult });
 							}
-                            if (interceptedErrorResult.isRejected()) {
-                                //rejected the error retry.  construct a "stopError" to abort axios retry functionality and return it.
-                                let stopError = new promise.retry.StopError("preRetryIntercept abort");
-                                (stopError as any)["interceptResult"] = interceptedErrorResult;
-                                return Promise.reject(stopError);
-                            } else {
-                                //do nothing special, ie the error gets returned back and axios retry functionality tries to kick in.
-                            }
+							if (interceptedErrorResult.isRejected()) {
+								//rejected the error retry.  construct a "stopError" to abort axios retry functionality and return it.
+								let stopError = new promise.retry.StopError("preRetryIntercept abort");
+								(stopError as any)["interceptResult"] = interceptedErrorResult;
+								return Promise.reject(stopError);
+							} else {
+								//do nothing special, ie the error gets returned back and axios retry functionality tries to kick in.
+							}
 						}
 						lastErrorResult = err;
 						return Promise.reject(err);
@@ -130,52 +265,52 @@ export class EzEndpointFunction<TSubmitPayload, TRecievePayload>{
 				return Promise.reject(err);
 			});
 	}
-    public get(
-        /**setting a key overrides the key put in ctor.requestOptions. */customRequestOptions?: Axios.AxiosXHRConfigBase<TRecievePayload>,
-        customOrigin: string|undefined = this.origin,
-        customPath: string | undefined = this.path
-    ): Promise<Axios.AxiosXHR<TRecievePayload>> {
+	public get(
+		/**setting a key overrides the key put in ctor.requestOptions. */customRequestOptions?: AxiosXHRConfigBase<TRecievePayload>,
+		customOrigin: string|undefined = this.origin,
+		customPath: string | undefined = this.path
+	): Promise<AxiosXHR<TRecievePayload>> {
 		log.debug("EzEndpointFunction .get() called");
-		return promise.retry<Axios.AxiosXHR<TRecievePayload>>(() => {
+		return promise.retry<AxiosXHR<TRecievePayload>>(() => {
 			let endpoint = customOrigin + customPath;
 			//log.debug("EzEndpointFunction axios.get", { endpoint });
 			//return axios.post<TRecievePayload>(endpoint, submitPayload, this.requestOptions) as any;
 
-			let finalRequestOptions: Axios.AxiosXHRConfigBase<TRecievePayload>;
+			let finalRequestOptions: AxiosXHRConfigBase<TRecievePayload>;
 			if (customRequestOptions == null || Object.keys(customRequestOptions).length === 0) {
 				finalRequestOptions = this.requestOptions;
 			} else {
 				finalRequestOptions = _.defaults({}, customRequestOptions, this.requestOptions);
 			}
 
-			return (axios.get<TRecievePayload>(endpoint, finalRequestOptions) as any as Promise<Axios.AxiosXHR<TRecievePayload>>)
+			return (axios.get<TRecievePayload>(endpoint, finalRequestOptions) as any as Promise<AxiosXHR<TRecievePayload>>)
 				.then((result) => {
 					return Promise.resolve(result);
-				}, (err: Axios.AxiosXHR<TRecievePayload>) => {
+				}, (err: AxiosXHR<TRecievePayload>) => {
 					//log.info(err);
 					if (err.status === 0 && err.statusText === "" && err.data === "" as any) {
 						//log.debug("EzEndpointFunction axios.get timeout.", { endpoint });
 						err.status = 524;
 						err.statusText = "A Timeout Occurred";
 						err.data = "Axios->EzEndpointFunction timeout." as any;
-                    }
-                    if (this.preRetryErrorIntercept != null) {
-                        let interceptedErrorResult = this.preRetryErrorIntercept(err);
-                        if (interceptedErrorResult == null || interceptedErrorResult.isRejected == null) {
-                            throw log.error("EzEndpointFunction GET interceptResult() did not return a promise", { interceptedErrorResult });
-                        }
-                        if (interceptedErrorResult.isFulfilled() === false) {
-                            throw log.error("EzEndpointFunction GET interceptResult() promise is not fulfilled", { interceptedErrorResult });
-                        }
-                        if (interceptedErrorResult.isRejected()) {
-                            //rejected the error retry.  construct a "stopError" to abort axios retry functionality and return it.
-                            let stopError = new promise.retry.StopError("preRetryIntercept abort");
-                            (stopError as any)["interceptResult"] = interceptedErrorResult;
-                            return Promise.reject(stopError);
-                        } else {
-                            //do nothing special, ie the error gets returned back and axios retry functionality tries to kick in.
-                        }
-                    }
+					}
+					if (this.preRetryErrorIntercept != null) {
+						let interceptedErrorResult = this.preRetryErrorIntercept(err);
+						if (interceptedErrorResult == null || interceptedErrorResult.isRejected == null) {
+							throw log.error("EzEndpointFunction GET interceptResult() did not return a promise", { interceptedErrorResult });
+						}
+						if (interceptedErrorResult.isFulfilled() === false) {
+							throw log.error("EzEndpointFunction GET interceptResult() promise is not fulfilled", { interceptedErrorResult });
+						}
+						if (interceptedErrorResult.isRejected()) {
+							//rejected the error retry.  construct a "stopError" to abort axios retry functionality and return it.
+							let stopError = new promise.retry.StopError("preRetryIntercept abort");
+							(stopError as any)["interceptResult"] = interceptedErrorResult;
+							return Promise.reject(stopError);
+						} else {
+							//do nothing special, ie the error gets returned back and axios retry functionality tries to kick in.
+						}
+					}
 					return Promise.reject(err);
 				});
 		}, this.retryOptions).catch((err: any) => {
