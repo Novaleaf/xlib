17f55e423e872aa8408b73a892f128e073e35da1
jasons <jasons@novaleaf.com>
2018-09-06 07:05:25 -0700

    update readme

--- README.md
+++ README.md
@@ -101,9 +101,17 @@ import xlib = require("xlib");
 
 the 10.x  branch (and semver) is a restructure of this project to take advantage of the now fully mature typescript ecosystem.  Prior to 10.x, to publish a typescript project was a tedious process.
 
-## future work
+## planned future work
 
-```xlib```'s core functionality has been used in production code since 2015.   While mostly stable since, a major refactor is expected to occur in the Dec 2018 timeframe, to better take advantage of improvements in typescript and javascript ecosystems.
+```xlib```'s core functionality has been used in production code since 2015.   While mostly stable since, here are the future plans:
+
+- **v.major,  explicit initialization**:   removal of all auto-initialization code (such as picking up environmental variables and configuring log levels)  and put them into options of an initialization function, something like: 
+
+	```typescript
+	import * as xlib from "xlib";
+	xlib.initialize({autoDetect:false,logLevel:"trace", env:"dev", tests:true});
+	```
+- **v.major, async**:  refactor to support async workflows by default
 
 --------
 # Why
--- src/core/diagnostics.ts
+++ src/core/diagnostics.ts
@@ -4,10 +4,11 @@
 //#region re-exports
 
 /** allows embeding mocha tests (unit tests) in your code, no-oping them if mocha is not present.  */
-import mockMocha = require("./diagnostics/mockmocha");
+import mockMocha = require( "./diagnostics/mockmocha" );
 mockMocha._initialize();
 
-export import logging = require("./diagnostics/logging");
+import logging = require( "./diagnostics/logging" );
+
 ///** obtain a stack trace at the callsite */
 //export import stacktrace = require("./diagnostics/stacktrace");
 
@@ -15,11 +16,11 @@ export import logging = require("./diagnostics/logging");
 
 
 /** cross-platform implementation of the nodejs module: http://nodejs.org/api/assert.html */
-export import assert = require("assert");
+export import assert = require( "assert" );
 //#endregion
 
-import exception = require("./exception");
-import environment = require("./environment");
+import exception = require( "./exception" );
+import environment = require( "./environment" );
 //import jsHelper = require("./runtime/jsHelper");
 
 /** thrown on race-check failures */
@@ -29,50 +30,50 @@ export class RaceCheckException extends exception.CorelibException { }
 export class DebugRaceCheck {
 	private _version = 0;
 	private _lockVersion = 0;
-	constructor(public debugText = "") { }
+	constructor( public debugText = "" ) { }
 
-	public enter: () => void = environment._ifDebug(function () {
-		if (this._lockVersion !== 0) {
+	public enter: () => void = environment._ifDebug( function () {
+		if ( this._lockVersion !== 0 ) {
 			//set lock version to -1 to cause the other thread caller to assert also
 			this._lockVersion = -1;
-			throw new RaceCheckException("DebugRaceCheck.Enter() failed!  Something else has this locked!  Additional Info=" + String(this.debugText));
+			throw new RaceCheckException( "DebugRaceCheck.Enter() failed!  Something else has this locked!  Additional Info=" + String( this.debugText ) );
 		}
 		this._lockVersion = this._version;
 		this._version++;
-	});
+	} );
 
-	public exit: () => void = environment._ifDebug(function () {
-		if (this._lockVersion !== this._version - 1) {
-			if (this._lockVersion === 0) {
-				throw new RaceCheckException("DebugRaceCheck.Exit() failed!  Already unlocked and we are calling unlock again!  Additional Info=" + String(this.debugText));
+	public exit: () => void = environment._ifDebug( function () {
+		if ( this._lockVersion !== this._version - 1 ) {
+			if ( this._lockVersion === 0 ) {
+				throw new RaceCheckException( "DebugRaceCheck.Exit() failed!  Already unlocked and we are calling unlock again!  Additional Info=" + String( this.debugText ) );
 			} else {
-				throw new RaceCheckException("DebugRaceCheck.Exit() failed!  internal state is corrupted, moste likely from multithreading  Additional Info=" + String(this.debugText));
+				throw new RaceCheckException( "DebugRaceCheck.Exit() failed!  internal state is corrupted, moste likely from multithreading  Additional Info=" + String( this.debugText ) );
 			}
 			//cause the assert to keep occuring if this failed
 			//return;
 		}
 
 		this._lockVersion = 0;
-	});
+	} );
 
 	/** quickly locks and unlocks */
-	public edit: () => void = environment._ifDebug(function () {
+	public edit: () => void = environment._ifDebug( function () {
 		this.enter();
 		this.exit();
-	});
+	} );
 
 	/** ensures that this raceCheck is not currently locked */
-	public poke: () => void = environment._ifDebug(function () {
+	public poke: () => void = environment._ifDebug( function () {
 		this._version++;
-		if (this._lockVersion !== 0) {
-			throw new RaceCheckException("DebugRaceCheck.Exit() failed!internal state is corrupted, moste likely from multithreading  Additional Info=" + String(this.debugText));
+		if ( this._lockVersion !== 0 ) {
+			throw new RaceCheckException( "DebugRaceCheck.Exit() failed!internal state is corrupted, moste likely from multithreading  Additional Info=" + String( this.debugText ) );
 		}
-	});
+	} );
 
 	public toString(): string {
-		if (environment.logLevel <= environment.LogLevel.DEBUG) {
+		if ( environment.logLevel <= environment.LogLevel.DEBUG ) {
 			var isEdit = this._lockVersion === 0 ? false : true;
-			return "version=" + String(this._version) + " isBeingEdited=" + String(isEdit);
+			return "version=" + String( this._version ) + " isBeingEdited=" + String( isEdit );
 		} else {
 			return "DebugRaceCheck is DISABLED when baselib.environment.logLevel > EnvLevel.DEBUG.  use baselib.concurrency.Lock if you need something in production";
 		}
